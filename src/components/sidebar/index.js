2d673cf6040c09b306e4cd942888def2716620a1
zariuz <nechetov@hotmail.com>
2020-06-10 18:10:54 +0300

    Rendering categories and filtering products

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BasketCart from './../basketCart';
 import Search from './../search';
+import Categories from './../categories';
 
 const Sidebar = () => {
   return (
     <div>
       <BasketCart />
       <Search />
+      <Categories />
     </div>
   );
 };
--- src/selectors.js
+++ src/selectors.js
@@ -2,12 +2,15 @@ import * as R from 'ramda';
 
 export const getPhoneById = (state, id) => R.prop(id, state.phones);
 
-export const getPhones = (state) => {
+export const getPhones = (state, ownProps) => {
+  const activeCategoryId = getActiveCategoryId(ownProps);
   const applySearch = (item) =>
-    R.includes(state.phonesPage.search, R.prop('name', item));
-
+    R.contains(state.phonesPage.search, R.prop('name', item));
+  const applyCategory = (item) =>
+    R.equals(activeCategoryId, R.prop('categoryId', item));
   const phones = R.compose(
     R.filter(applySearch),
+    R.when(R.always(activeCategoryId), R.filter(applyCategory)),
     R.map((id) => getPhoneById(state, id))
   )(state.phonesPage.ids);
   return phones;
@@ -30,3 +33,8 @@ export const getTotalBasketPrice = (state) => {
 
   return totalPrice;
 };
+
+export const getCategories = (state) => R.values(state.categories);
+
+export const getActiveCategoryId = (ownProps) =>
+  R.path(['match', 'params', 'id'], ownProps);
