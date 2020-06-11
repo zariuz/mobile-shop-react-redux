9a9988a94115dda952d9ecaaa0714bb387f6ff7c
zariuz <nechetov@hotmail.com>
2020-06-11 23:52:22 +0300

    Preparing data for basket page

--- src/selectors.js
+++ src/selectors.js
@@ -38,3 +38,20 @@ export const getCategories = (state) => R.values(state.categories);
 
 export const getActiveCategoryId = (ownProps) =>
   R.path(['match', 'params', 'id'], ownProps);
+
+export const getBasketPhonesWithCount = (state) => {
+  const phoneCount = (id) =>
+    R.compose(
+      R.length,
+      R.filter((basketId) => R.equals(id, basketId))
+    )(state.basket);
+  const phoneWithCount = (phone) =>
+    R.assoc('count', phoneCount(phone.id), phone);
+  const uniqueIds = R.uniq(state.basket);
+  const phones = R.compose(
+    R.map(phoneWithCount),
+    R.map((id) => getPhoneById(state, id))
+  )(uniqueIds);
+
+  return phones;
+};
